a81c96cc4d86fd519ef15fea3503c948b0661288
seanngpack <seanngpack@gmail.com>
2020-02-21 15:26:41 -0500

    added flask connection to local databse

--- app/db.py
+++ app/db.py
@@ -4,18 +4,29 @@ from flask import current_app, g
 from flask.cli import with_appcontext
 
 
-def get_db():
+def init_db():
     if 'db' not in g:
         mysql = MySQL()
         # current_app.config['MYSQL_DATABASE_USER'] = 'jay'
         # current_app.config['MYSQL_DATABASE_PASSWORD'] = 'jay'
         current_app.config['MYSQL_DATABASE_DB'] = 'HW4'
+        current_app.config['MYSQL_DATABASE_USER'] = 'root'
         current_app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+        mysql.init_app(current_app)
         conn = mysql.connect()
         g.db = conn
-        g.db.cursor = conn.cursor
 
     return g.db
+
+def fetch_test():
+    '''do a cursor.execute command in here
+    '''
+    command = 'SELECT * FROM EPL_stadiums'
+    cursor = g.db.cursor()
+    cursor.execute(command)
+    data = cursor.fetchall()
+    print(data)
+    return data
 
 
 def close_db(e=None):
@@ -26,7 +37,15 @@ def close_db(e=None):
 
 def init_app(app):
     app.teardown_appcontext(close_db)
-    # app.cli.add_command(init_db_command)
+    app.cli.add_command(test_fetch_command)
+
+@click.command('init-db')
+@with_appcontext
+def test_fetch_command():
+    """start the database with flask and then do a fetch"""
+    init_db()
+    click.echo('Initialized the database.')
+    fetch_test()
 
 # TODO: missing init_db functions, implement later where the create table
 # command is in sql and the init_db calls that sql file.
